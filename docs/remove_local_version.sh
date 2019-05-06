b88687e701a3b968f43d18a5f1d51cabc415ea81
Diego Prada <prada.gracia@gmail.com>
2019-05-06 18:50:59 -0500

    update to include autogenerated documentation

--- docs/remove_local_version.sh
+++ docs/remove_local_version.sh
@@ -2,5 +2,6 @@
 
 echo "Removing local version"
 
+make clean
 rm -rf _build
 rm -rf api/generated
--- docs/update_gh-pages.sh
+++ docs/update_gh-pages.sh
@@ -4,23 +4,22 @@ if git diff-index --quiet HEAD --; then
 
     echo "Updating gh-pages branch"
 
+    bash build_local_version.sh
+
     cd ..
     git checkout gh-pages
-    git rm -rf *
-    git checkout master docs/*
-    cd docs
-    make html
-    mv -fv _build/html/* ./../.
-    make clean
-    rm -rf _build
-    cd ..
-    git rm -rf docs
+    rm -rf *
+    git checkout master docs/_build/html/*
+    mv -fv docs/_build/html/* .
+    rm -rf docs
     touch .nojekyll
     git add --all
     git commit -a -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`"
     git push origin gh-pages
     git checkout master
+
     cd docs
+    bash remove_local_version.sh
 
 else
 
