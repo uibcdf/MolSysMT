2cff60a417a7a9334570d93078f4814bd1206af3
Diego Prada <prada.gracia@gmail.com>
2019-11-01 23:59:29 -0600

    in process of importing mmtfs as compositions

--- molmodmt/native/io/composition/mmtf_Decoder.py
+++ molmodmt/native/io/composition/mmtf_Decoder.py
@@ -2,8 +2,8 @@
 def import_mmtf_Decoder(item):
 
     from molmodmt.native.composition import Composition
-    from molmodmt.native.elements import Atom, Group, Component, Chain, Molecule, Entity, BioAssembly
-    from molmodmt.native.elements.molecules import Ion, Water, Cosolute, SmallMolecule, Peptide, Protein, DNA, RNA
+    from molmodmt.native import elements
+    from molmodmt.topology.groups_classification import MMTFDecoder_group_to_group_class
 
     tmp_item = Composition()
 
@@ -12,16 +12,21 @@ def import_mmtf_Decoder(item):
     tmp_item.chain = []
 
     for id, index in zip(range(item.num_atoms), item.atom_id_list):
-        atom = Atom(id=id, index=index)
+        atom = element.Atom(id=id, index=index)
         tmp_item.atom.append(atom)
 
-    for id, index in zip(range(item.num_atoms), item.group_id_list):
-        group = Group(id=id, index=index)
-        tmp_item.group.append(group)
+    for id, index, id_type in zip(range(item.num_groups), item.group_id_list, item.group_type_list):
+        group_mmtf = item.group_list[id_type]
+        group_class = MMTFDecoder_group_to_group_class(group_mmtf)
+        if group_class is None:
+            group = element.Group(id=id, index=index)
+        else:
+            group = getattr(element.groups,group_class)(id=id, index=index)
 
     for id, index, name in zip(range(item.num_chains), item.chain_id_list, item.chain_name_list):
-        chain = Chain(id=id, index=index, name=name)
+        chain = element.Chain(id=id, index=index, name=name)
         tmp_item.chain.append(chain)
+
 
     # Inter group bonds
     #if hasattr(mmtf, 'bond_atom_list'):
--- molmodmt/topology.py
+++ molmodmt/topology.py
@@ -1,44 +1,5 @@
-from .utils.exceptions import *
-
-from mdtraj.core.residue_names import _AMINO_ACID_CODES
-from mdtraj.core.residue_names import _WATER_RESIDUES
-from mdtraj.core.residue_names import _SOLVENT_TYPES
-
-amino_acid_codes = _AMINO_ACID_CODES
-protein_residues = frozenset(_AMINO_ACID_CODES.keys())
-rna_residues = frozenset(['A', 'G', 'C', 'U', 'I'])
-dna_residues = frozenset(['DA', 'DG', 'DC', 'DT', 'DI'])
-water_residues = _WATER_RESIDUES
-solvent_residues = _SOLVENT_TYPES
-ion_residues = frozenset([ii for ii in solvent_residues if ii not in water_residues])
-
-def residue_name_to_molecule_type(residue_name):
-
-    tmp_type='unknown'
-
-    if residue_name in water_residues:
-        tmp_type='water'
-    elif residue_name in protein_residues:
-        tmp_type='protein'
-    elif residue_name in ion_residues:
-        tmp_type='ion'
-    elif residue_name in dna_residues:
-        tmp_type='dna'
-    elif residue_name in rna_residues:
-        tmp_type='rna'
-
-    return tmp_type
+from .utils.groups import groups_classification as groups_classification
+from .utils.groups import groups_classification as residues_classification
 
 
-def is_water(residue_name):
-    return (residue_name in water_residues)
-
-def is_ion(residue_name):
-    return (residue_name in ion_residues)
-
-def is_aminoacid(residue_name):
-    return (residue_name in protein_residues)
-
-def is_nucleotide(residue_name):
-    return ((residue_name in dna_residues) or (residue_name in rna_residues))
 
