20aca91bdf7fb350830b92d2a6af2fe28e708533
Diego Prada <prada.gracia@gmail.com>
2019-11-11 18:39:31 -0600

    more in composition io

---
+++ molmodmt/forms/classes/api_networkx_Graph.py
@@ -0,0 +1,27 @@
+from os.path import basename as _basename
+from molmodmt.utils.exceptions import *
+from networkx import Graph as _networkx_Graph
+
+form_name=_basename(__file__).split('.')[0].replace('api_','').replace('_','.')
+
+is_form={
+    'networkx.Graph': form_name,
+    _networkx_Graph : form_name
+}
+
+def select_with_MDTraj(item, selection):
+
+    raise NotImplementedError
+
+def extract_subsystem(item, atom_indices='all', frame_indices='all'):
+
+    if (atom_indices is 'all') and (frame_indices is 'all'):
+        return item
+    else:
+        raise NotImplementedError
+
+def duplicate(item):
+
+    return item.copy()
+
+
