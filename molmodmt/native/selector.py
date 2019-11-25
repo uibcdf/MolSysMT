e8f4113bcd8f50a099db89e88634ec86798fc671
Diego Prada <prada.gracia@gmail.com>
2019-11-25 16:55:22 -0600

    pandas query is using external varibles now -fixed-

--- molmodmt/native/selector.py
+++ molmodmt/native/selector.py
@@ -1,6 +1,17 @@
 
 def query_dataframe(item, selection='all'):
 
+    if '@' in selection:
+        from inspect import stack
+        count = 0
+        for stack_frame in stack():
+            count +=1
+            if stack_frame.function == 'select':
+                break
+        var_names = [name[1:] for name in selection.split(' ') if name.startswith('@')]
+        for var_name in var_names:
+            globals()[var_name]=stack()[count][0].f_globals[var_name]
+
     selection = selection.replace(".","_")
     item.columns = [column.replace(".", "_") for column in item.columns] 
     atom_indices = item.query(selection).index.to_numpy()
@@ -16,12 +27,6 @@ def parse(selection='all'):
 
 def dataframe_select(item, selection='all'):
 
-    import inspect
-    print("hola")
-    print(inspect.stack()[1][0])
-    print(inspect.stack()[1][0].f_globals['lista'])
-
     parsed_selection = parse(selection)
     atom_indices = query_dataframe(item, selection=parsed_selection)
     return atom_indices
-
