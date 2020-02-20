a3b28c5d0e16bf40e9ffc466447f793372b7debb
Diego Prada <prada.gracia@gmail.com>
2020-02-20 15:17:54 -0600

    mdtraj.Topology to molmodmt.DataFrame and molmodmt.MolMod implemented in its first version

--- molmodmt/native/io/dataframe/classes/mdtraj_Topology.py
+++ molmodmt/native/io/dataframe/classes/mdtraj_Topology.py
@@ -1,49 +1,101 @@
 
 def from_mdtraj_Topology(item, atom_indices='all', frame_indices='all'):
 
-
     from molmodmt.native import DataFrame
-    from pandas import Series
     from molmodmt.forms.classes.api_molmodmt_DataFrame import extract_subsystem
+    from numpy import empty, array, arange, reshape, where, unique, nan, sort
+    from molmodmt.elements.group import name_to_type as group_name_to_group_type
+    from networkx import empty_graph, connected_components
 
     tmp_item = DataFrame()
 
-    mdtraj_DataFrame, bonds_list = item.to_dataframe()
+    n_atoms = item.n_atoms
 
-    tmp_item['atom.index'] = Series(mdtraj_DataFrame.index).values
-    tmp_item['atom.name'] = mdtraj_DataFrame['name'].values
-    tmp_item['atom.id'] = mdtraj_DataFrame['serial'].values
-    tmp_item['atom.type'] = mdtraj_DataFrame['element'].values
+    mdtraj_dataframe, mdtraj_bonds = item.to_dataframe()
 
-    tmp_item['group.index'] = Series(atom.residue.index for atom in item.atoms).values
-    tmp_item['group.name'] = mdtraj_DataFrame['resName'].values
-    tmp_item['group.id'] = mdtraj_DataFrame['resSeq'].values
-    #tmp_item['group.type']
+    tmp_item["atom.index"] = list(mdtraj_dataframe.index)
+    tmp_item["atom.id"] = mdtraj_dataframe["serial"]
+    tmp_item["atom.name"] = mdtraj_dataframe["name"]
+    tmp_item["atom.type"] = mdtraj_dataframe["element"]
 
-    #tmp_item['component.index']
-    #tmp_item['component.name']
-    #tmp_item['component.id']
-    #tmp_item['component.type']
+    tmp_item["group.id"] = mdtraj_dataframe["resSeq"]
+    tmp_item["group.name"] = mdtraj_dataframe["resName"]
 
-    tmp_item['chain.index'] = Series(atom.residue.chain.index for atom in item.atoms).values
-    #tmp_item['chain.name']
-    tmp_item['chain.id'] = mdtraj_DataFrame['chainID'].values
-    #tmp_item['chain.type']
+    tmp_item["chain.id"] = mdtraj_dataframe["chainID"]
 
-    #tmp_item['entity.index']
-    #tmp_item['entity.name']
-    #tmp_item['entity.id']
-    #tmp_item['entity.type']
+    del(mdtraj_dataframe)
 
-    #tmp_item['bioassembly.index']
-    #tmp_item['bioassembly.name']
-    #tmp_item['bioassembly.id']
-    #tmp_item['bioassembly.type']
+    group_type_array = empty(n_atoms, dtype=object)
 
-    tmp_item.set_index(tmp_item['atom.index'].values)
+    tmp_item["group.type"] = list(map(group_name_to_group_type,tmp_item["group.name"]))
 
-    tmp_item = extract_subsystem(tmp_item, atom_indices=atom_indices, frame_indices=frame_indices)
+    G = empty_graph(n_atoms)
+
+    atom_bonded_atom_indices_array = empty(n_atoms, dtype=object)
+
+    for mdtraj_bond in mdtraj_bonds:
+        G.add_edge(int(mdtraj_bond[0]), int(mdtraj_bond[1]))
+
+    for atom_index in range(n_atoms):
+        aux_list = list(G.neighbors(atom_index))
+        if len(aux_list)>1:
+            aux_list = list(sort(aux_list))
+        atom_bonded_atom_indices_array[atom_index] = aux_list
+
+    tmp_item["atom.bonded_atom_indices"] = atom_bonded_atom_indices_array
+    del(atom_bonded_atom_indices_array)
+
+    atom_indices_per_component = list(connected_components(G))
+    del(G)
+
+    component_index_array = empty(n_atoms, dtype=int)
+    #component_name_array = empty(n_atoms, dtype=object)
+    #component_id_array = empty(n_atoms, dtype=int)
+    #component_type_array = empty(n_atoms, dtype=object)
+
+    component_index = 0
+
+    for atom_indices_of_component in atom_indices_per_component:
+        for atom_index in atom_indices_of_component:
+
+            component_index_array[atom_index] = component_index
+
+        component_index += 1
+
+    tmp_item["component.index"] = component_index_array
+
+    del(component_index_array)
+
+    group_index_array = empty(n_atoms, dtype=int)
+    chain_index_array = empty(n_atoms, dtype=int)
+
+    for atom in item.atoms:
+        atom_index = atom.index
+        group_index_array[atom_index] = atom.residue.index
+        chain_index_array[atom_index] = atom.residue.chain.index
+
+    tmp_item["group.index"] = group_index_array
+    tmp_item["chain.index"] = chain_index_array
+
+    del(group_index_array, chain_index_array)
+
+    bioassembly_index_array = empty(n_atoms, dtype=int)
+    bioassembly_index_array.fill(0)
+    tmp_item["bioassembly.index"] = bioassembly_index_array
+
+    del(bioassembly_index_array)
+
+    molecule_index_array = empty(n_atoms, dtype=int)
+    molecule_index_array.fill(0)
+    tmp_item["molecule.index"] = molecule_index_array
+
+    del(molecule_index_array)
+
+    entity_index_array = empty(n_atoms, dtype=int)
+    entity_index_array.fill(0)
+    tmp_item["entity.index"] = entity_index_array
+
+    del(entity_index_array)
 
     return tmp_item
-
 
