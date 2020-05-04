70157b3aeefbd72adb3d160b3072f9fbaee322e2
Diego Prada <prada.gracia@gmail.com>
2020-05-04 18:54:43 -0500

    Fixex small bug with topology variable

--- molsysmt/native/io/molsys/files/pdb.py
+++ molsysmt/native/io/molsys/files/pdb.py
@@ -4,7 +4,7 @@ def from_pdb(item, atom_indices='all', frame_indices='all'):
     from molsysmt import convert
 
     tmp_item = MolSys()
-    tmp_item.composition = convert(topology, to_form='molsysmt.Composition', selection=atom_indices, frame_indices=frame_indices)
+    tmp_item.composition = convert(item, to_form='molsysmt.Composition', selection=atom_indices, frame_indices=frame_indices)
     tmp_item.trajectory = convert(item, to_form='molsysmt.Trajectory', selection=atom_indices, frame_indices=frame_indices)
     #tmp_item.topography = None
     #tmp_item.structure = None
