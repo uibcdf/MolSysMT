7042626d7197c4a9087770e722d70c42e0dece1d
Liliana M. Moreno Vargas <lm.moreno.vargas@gmail.com>
2020-06-02 17:48:10 -0500

    Nueva propiedad fisicoquimica añadida: Abraham and Leo

--- molsysmt/chemphys_properties/residues/hydrophobicity.py
+++ molsysmt/chemphys_properties/residues/hydrophobicity.py
@@ -84,14 +84,28 @@ Kyte ={
 }
 
 
-#
+# Abraham D.J., Leo A.J.; Proteins: Structure, Function and Genetics 2:130-152(1987).
 
+Abraham ={
+    'ALA':  0.440,
+    'ARG': -2.420,
+    'ASN': -1.320,
+    'ASP': -0.310,
+    'CYS':  0.580,
+    'GLN': -0.710,
+    'GLU': -0.340,
+    'GLY':  0.000,
+    'HIS': -0.010,
+    'ILE':  2.460,
+    'LEU':  2.460,
+    'LYS': -2.450,
+    'MET':  1.100,
+    'PHE':  2.540,
+    'PRO':  1.290,
+    'SER': -0.840,
+    'THR': -0.410,
+    'TRP':  2.560,
+    'TYR':  1.630,
+    'VAL':  1.730
 
-
-
-
-
-
-
-
-
+}
