8cd80a619c9ceb2f0ec49206ff3791f1ca25a50d
Diego Prada <prada.gracia@gmail.com>
2021-10-26 16:36:21 -0500

    refact

--- molsysmt/tests/basic/get/test_get_file_h5.py
+++ molsysmt/tests/basic/get/test_get_file_h5.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 def test_get_file_h5_1():
-    molsys = msm.demo.files['pentalanine.h5']
+    molsys = msm.demo.pentalanine['traj.h5']
     n_atoms, n_frames = msm.get(molsys, target='system', n_atoms=True, n_frames=True)
     assert (n_atoms==62) and (n_frames==5000)
 
