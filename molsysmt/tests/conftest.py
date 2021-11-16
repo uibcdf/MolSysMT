1a891cb5c42740f9ec5da40608b62d82568fad50
Diego Prada <prada.gracia@gmail.com>
2021-11-16 15:53:00 -0600

    Removing contest.py file from tests

--- molsysmt/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-def pytest_sessionstart(session):
-    """
-    Called after the Session object has been created and
-    before performing collection and entering the run test loop.
-    """
-
-    import molsysmt as msm
-    msm.tools.nglview.adding_molsysmt()
-
