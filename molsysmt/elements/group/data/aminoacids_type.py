0ea29285fafa2774c216fbe1e4ac63cb0a11cc59
Diego Prada <prada.gracia@gmail.com>
2022-03-17 15:35:23 -0600

    refact

---
+++ molsysmt/elements/group/data/aminoacids_type.py
@@ -0,0 +1,27 @@
+
+aminoacids_type = {
+
+        'ALA': 'A',
+        'ARG': 'R',
+        'ASN': 'N',
+        'ASP': 'D',
+        'CYS': 'C',
+        'GLU': 'E',
+        'GLN': 'Q',
+        'GLY': 'G',
+        'HIS': 'H',
+        'HYP': 'O',
+        'ILE': 'I',
+        'LEU': 'L',
+        'LYS': 'K',
+        'MET': 'M',
+        'PHE': 'F',
+        'PRO': 'P',
+        'GLP': 'U',
+        'SER': 'S',
+        'THR': 'T',
+        'TRP': 'W',
+        'TYR': 'Y',
+        'VAL': 'V'
+
+        }
