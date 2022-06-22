628b037ab4a781d28cd4c468eccbc91051fd967b
Daniel-Ibarrola <dis96051@gmail.com>
2022-06-22 15:24:07 -0500

    Temporarily disabled the tests unwrap_trajectoryDict and wrap_trajectoryDict

--- molsysmt/tests/pbc/wrap_to_pbc/test_wrap_to_pbc_molsysmt_TrajectoryDict.py
+++ molsysmt/tests/pbc/wrap_to_pbc/test_wrap_to_pbc_molsysmt_TrajectoryDict.py
@@ -9,14 +9,15 @@ import math
 
 # Distance between atoms in space and time
 
-
-def test_wrap_to_pbc_molsysmt_TrajectoryDict_1():
-    molsys = msm.convert(msm.demo['two LJ particles']['traj.trjpk'], to_form='molsysmt.TrajectoryDict')
-    distance = msm.structure.get_distances(molsys, selection=0, selection_2=1, pbc=True)
-    molsys_wrapped = msm.pbc.wrap_to_pbc(molsys)
-    distance_wrapped = msm.structure.get_distances(molsys_wrapped, selection=0, selection_2=1, pbc=True)
-    check_distances = np.allclose(distance[:,:,:], distance_wrapped[:,:,:])
-    box_length = msm.get(molsys_wrapped, element='system', box_lengths=True)[0,0]
-    check_limits_wrapped = ( box_length >= (np.max(molsys_wrapped['coordinates']) - np.min(molsys_wrapped['coordinates'])))
-    check_limits = ( box_length <= (np.max(molsys['coordinates']) - np.min(molsys['coordinates'])))
-    assert check_limits and check_distances and check_limits_wrapped
+# TODO: This test does work because TrajectoryDict was moved to Freezer. While that gets
+# fixed the test will be commented
+# def test_wrap_to_pbc_molsysmt_TrajectoryDict_1():
+#     molsys = msm.convert(msm.demo['two LJ particles']['traj.trjpk'], to_form='molsysmt.TrajectoryDict')
+#     distance = msm.structure.get_distances(molsys, selection=0, selection_2=1, pbc=True)
+#     molsys_wrapped = msm.pbc.wrap_to_pbc(molsys)
+#     distance_wrapped = msm.structure.get_distances(molsys_wrapped, selection=0, selection_2=1, pbc=True)
+#     check_distances = np.allclose(distance[:,:,:], distance_wrapped[:,:,:])
+#     box_length = msm.get(molsys_wrapped, element='system', box_lengths=True)[0,0]
+#     check_limits_wrapped = ( box_length >= (np.max(molsys_wrapped['coordinates']) - np.min(molsys_wrapped['coordinates'])))
+#     check_limits = ( box_length <= (np.max(molsys['coordinates']) - np.min(molsys['coordinates'])))
+#     assert check_limits and check_distances and check_limits_wrapped
