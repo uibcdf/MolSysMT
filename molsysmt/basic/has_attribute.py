c3db231a27eb85beeb63d6ea5d78bd9819831768
Daniel-Ibarrola <dis96051@gmail.com>
2022-06-30 13:33:03 -0500

    Added decorator to info and removed check argument

--- molsysmt/basic/has_attribute.py
+++ molsysmt/basic/has_attribute.py
@@ -2,6 +2,7 @@ from molsysmt._private.exceptions import *
 from molsysmt._private.digestion import *
 from molsysmt._private.lists_and_tuples import is_list_or_tuple
 
+# TODO: this function has unresolved references. Also, variable item is not defined anywhere.
 def has_attribute(molecular_system, attribute, check=True):
 
     from . import where_is_attribute, is_molecular_system
@@ -14,7 +15,6 @@ def has_attribute(molecular_system, attribute, check=True):
 
         if not is_attribute(attribute):
             raise WrongAttributeError(attribute)
-
 
     output_item, output_form = where_is_attribute(item, attribute, check=False)
 
