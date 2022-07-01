57111589b73461ccbf67aa49d5f57cce1532941f
Diego Prada <prada.gracia@gmail.com>
2022-06-30 23:00:38 -0500

    Tests passed after including file_trjpk and molsysmt_TrajectoryDict

--- molsysmt/tests/pbc/unwrap/test_unwrap_molsysmt_TrajectoryDict.py
+++ molsysmt/tests/pbc/unwrap/test_unwrap_molsysmt_TrajectoryDict.py
@@ -13,7 +13,7 @@ def test_unwrap_molsysmt_TrajectoryDict_1():
     molsys = msm.convert(msm.demo['two LJ particles']['traj.trjpk'], to_form='molsysmt.TrajectoryDict')
     molsys_wrapped = msm.pbc.wrap_to_pbc(molsys)
     molsys_unwrapped = msm.pbc.unwrap(molsys_wrapped)
-    box_length = msm.get(molsys_wrapped, target='system', box_lengths=True)[0,0]
+    box_length = msm.get(molsys_wrapped, element='system', box_lengths=True)[0,0]
     check_limits_wrapped = ( box_length >= (np.max(molsys_wrapped['coordinates']) - np.min(molsys_wrapped['coordinates'])))
     check_limits = ( box_length <= (np.max(molsys['coordinates']) - np.min(molsys['coordinates'])))
     check_limits_unwrapped = ( box_length <= (np.max(molsys_unwrapped['coordinates']) - np.min(molsys['coordinates'])))
