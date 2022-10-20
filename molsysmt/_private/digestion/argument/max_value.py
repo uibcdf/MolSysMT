21858fbd744280d9dc7614780aafba68dfb85c48
Diego Prada <prada.gracia@gmail.com>
2022-10-20 16:46:23 -0500

    In proces

--- molsysmt/_private/digestion/argument/max_value.py
+++ molsysmt/_private/digestion/argument/max_value.py
@@ -7,10 +7,11 @@ def digest_max_value(max_value, caller=None):
     if max_value is None:
         return None
 
-    if isinstance(max_value, [int, float]):
-        return max_value
-
     if puw.is_quantity(max_value):
+        max_value = puw.get_value(max_value)
+
+    if isinstance(max_value, (int, float)):
         return max_value
 
     raise ArgumentError('max_value', value=max_value, caller=caller, message=None)
+
--- molsysmt/thirds/nglview/color_by_value.py
+++ molsysmt/thirds/nglview/color_by_value.py
@@ -13,23 +13,29 @@ def color_by_value(view, values, element='group', selection='all', cmap=None,
     from molsysmt.basic import select
     from matplotlib.colors import Normalize, to_hex
 
-    if element=='group':
-        elements_selection = select(view, element='group', selection=selection, syntax=syntax, to_syntax='NGLview')
-    else:
-        raise ValueError()
-
     if min_value is None:
         min_value = min(values)
     if max_value is None:
         max_value = max(values)
 
-
     norm = Normalize(vmin=min_value,vmax=max_value)
-    scheme = _ColorScheme([[to_hex(cmap(norm(ii))), jj] for ii,jj in zip(values, elements_selection.split(' '))], label='user')
+
+    if element=='group':
+        elements_selection = select(view, element='group', selection=selection, syntax=syntax, to_syntax='NGLview')
+        scheme = _ColorScheme([[to_hex(cmap(norm(ii))), jj] for ii,jj in zip(values, elements_selection.split(' '))], label='user')
+    elif element=='atom':
+        elements_selection = select(view, element='atom', selection=selection, syntax=syntax, to_syntax='NGLview')
+        scheme = _ColorScheme([[to_hex(cmap(norm(ii))), '@'+jj] for ii,jj in zip(values, elements_selection[1:].split(','))], label='user')
+    else:
+        raise ValueError()
+    
     if representation=='surface':
         view.add_surface(selection=elements_selection, color=scheme)
     elif representation=='cartoon':
-        view.clear()
         view.add_cartoon(selection=elements_selection, color=scheme)
+    elif representation=='licorice':
+        view.add_licorice(selection=elements_selection, color=scheme)
+    elif representation=='ball_and_stick':
+        view.add_ball_and_stick(selection=elements_selection, color=scheme)
 
     pass
