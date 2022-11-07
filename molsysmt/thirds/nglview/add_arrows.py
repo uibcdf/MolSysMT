d8c2c30ccc03179975e08001be8c06f0118b6141
Diego Prada-Gracia <prada.gracia@gmail.com>
2022-11-06 18:53:21 -0600

    Adding first version of function molsysmt.thirds.nglview.add_arrows()

---
+++ molsysmt/thirds/nglview/add_arrows.py
@@ -0,0 +1,93 @@
+from molsysmt._private.digestion import digest
+from molsysmt._private.variables import is_all
+
+# https://github.com/arose/ngl/blob/master/doc/usage/selection-language.md
+
+@digest()
+def add_arrows(positions, arrows, color=[0.6, 0.6, 0.6], radius='0.2 angstroms'):
+    """Adding arrows to a view.
+
+    A list of arrows can be added to an NGL view (NGLWidget).
+
+    Parameters
+    ----------
+
+    view: nglview.NGLWidget
+       A view of the molecular system as an nglview.NGLWidget native object.
+
+    positions: Quantity (value:numpy.ndarray, dimensionality:['L']=1)
+       Positions of the points of origin for the set of arrows. A quantity with length dimensions is required (['L']=1), the value must be a numpy ndarray with shape [n_arrows, 3] (n_arrows is the number of arrows to be added).
+
+    arrows: Quantity (value:numpy.ndarray, dimensionality:['L']=1)
+       Vectors to be added as arrows. A quantity with length dimensions is required (['L']=1), the value must be a numpy ndarray with shape [n_arrows, 3] (n_arrows is the number of arrows to be added).
+
+    color: list, tuple, default=[0.6, 0.6, 0.6]
+       RGB color code of the arrows.
+
+    radius: Quantity (value:float, dimensionality:['L']=1), default='0.2 angstroms'
+       Radius of the arrows.
+
+    Returns
+    -------
+    None
+        The method modifies an nglview.NGLWidget object to add a list of arrows.
+
+    Examples
+    --------
+    >>> import molsysmt as msm
+    >>> from molsysmt import pyunitwizard as puw
+    >>> import numpy as np
+    >>> molecular_system = msm.convert('181L', selection='molecule_type=="protein"')
+    >>> positions = msm.get(molecular_system, element='atom', selection='atom_name=="CA"', coordinates=True)
+    >>> arrows = puw.quantity(np.ones([positions.shape[0],3]), 'angstroms')
+    >>> view = msm.view(molecular_system)
+    >>> msm.thirds.add_arrows(view, positions, arrows)
+    >>> view
+
+    See Also
+    --------
+    :func:`molsysmt.basic.view`, :func:`molsysmt.basic.select`
+
+    Notes
+    -----
+
+    Have a look to the `YYY`_.
+
+    .. YYY:
+       https://uibcdf.org/molsysmt
+
+
+    """
+
+    positions = puw.get_value(positions, to_unit='angstroms')
+    positions = puw.get_value(positions, to_unit='angstroms')
+    radius = puw.get_value(radius, to_unit='angstroms')
+
+    n_arrows=positions.shape[0]
+    end_arrows = positions+arrows
+    
+    for ii in range(n_arrows):
+    
+        kwargs = {'position1':positions[ii].tolist(),
+                  'position2':end_arrows[ii].tolist(),
+                  'color': color,
+                  'radius': [radius]}
+                        
+        msg = view._get_remote_call_msg("addBuffer",
+                                        target="Widget",
+                                        args=["arrow"],
+                                        kwargs=kwargs,
+                                        fire_embed=True)
+
+        def callback(widget, msg=msg):
+            widget.send(msg)
+
+        callback._method_name = 'addBuffer'
+        callback._ngl_msg = msg
+
+        view._ngl_displayed_callbacks_before_loaded.append(callback)
+
+        view._ngl_msg_archive.append(msg)
+
+    pass
+
