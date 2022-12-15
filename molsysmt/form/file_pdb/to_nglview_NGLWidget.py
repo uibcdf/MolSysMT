b6d4ce7f5e2668f98d16e1e2acfe4f2b559c68de
Diego Prada-Gracia <prada.gracia@gmail.com>
2022-12-15 09:07:05 -0600

    In process

--- molsysmt/form/file_pdb/to_nglview_NGLWidget.py
+++ molsysmt/form/file_pdb/to_nglview_NGLWidget.py
@@ -4,7 +4,7 @@ from molsysmt._private.digestion import digest
 def to_nglview_NGLWidget(item, atom_indices='all', structure_indices='all'):
 
     from . import to_string_pdb_text
-    from .string_pdb_text import to_nglview_NGLWidget as string_pdb_text_to_nglview_NGLWidget
+    from ..string_pdb_text import to_nglview_NGLWidget as string_pdb_text_to_nglview_NGLWidget
 
     tmp_item = to_string_pdb_text(item)
     tmp_item = string_pdb_text_to_nglview_NGLWidget(tmp_item, atom_indices=atom_indices,
