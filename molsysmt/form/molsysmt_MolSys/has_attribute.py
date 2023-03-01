181235ee65d72699b2068e8b7f6c5d59fc0d0e11
Diego Prada-Gracia <prada.gracia@gmail.com>
2023-03-01 09:48:08 -0600

    In process

--- molsysmt/form/molsysmt_MolSys/has_attribute.py
+++ molsysmt/form/molsysmt_MolSys/has_attribute.py
@@ -23,9 +23,19 @@ def has_attribute(molecular_system, **kwargs):
                 'component_index', 'component_id', 'component_name', 'component_type',
                 'molecule_index', 'molecule_id', 'molecule_name', 'molecule_type',
                 'chain_index', 'chain_id', 'chain_name', 'chain_type',
-                'entity_index', 'entity_id', 'entity_name', 'entity_type']:
+                'entity_index', 'entity_id', 'entity_name', 'entity_type',
+                'n_atoms', 'n_groups', 'n_components', 'n_chains', 'n_molecules',
+                'n_entities', 'n_aminoacids', 'n_nucleotides', 'n_ions', 'n_waters',
+                'n_waters', 'n_small_molecules', 'n_peptides', 'n_proteins', 'n_dnas', 'n_rnas',
+                'n_lipids', 'n_oligosaccharides', 'n_saccharides']:
             if molecular_system.topology.atoms_data_frame.shape[0]:
                 output = True 
+
+        if argument in ['bond_index', 'bond_id', 'bond_name', 'bond_type',
+                'bond_order', 'bond_atoms']:
+            if molecular_system.topology.atoms_data_frame.shape[0]:
+                output = True 
+
 
 
 
