71f018caba4ed8bce1fe670b42f710edf9e7da85
Diego Prada <prada.gracia@gmail.com>
2023-05-18 16:16:10 -0600

    Añadiendo retos y preguntas

--- molsysmt/lib/structure/translate.py
+++ molsysmt/lib/structure/translate.py
@@ -43,46 +43,48 @@ output=None
 @nb.njit(make_numba_signature(arguments,output))
 def translate(coordinates, translation, atom_indices=None, structure_indices=None):
 
-    if translation.shape[0]==1:
-        iter_structures_t=infinite_sequence(0,0)
-    else:
-        iter_structures_t=infinite_sequence(0,1)
-
     new_coordinates=coordinates.copy()
 
     n_structures, n_atoms = coordinates.shape[:-1]
+    n_structures_translation, n_atoms_translation = translation.shape[:-1]
+    single_structure_translation = (single_structure_translation==1)
 
     if (atom_indices is None) and (structure_indices is None):
 
-        iter_atoms = range(n_atoms)
-        iter_structures = range(n_structures)
+        ss=0
+        for ii in range(n_structure):
+            for jj in range(n_atoms):
+                coordinates[ii,jj,:]+=translation[ii,jj,:]
+            if not single_structure_translation:
+                ss+=1
 
-    elif (atom_indices is None) and (structure_indices is not None):
+    elif (atom_indices is None):
 
-        iter_atoms = range(n_atoms)
-        iter_structures = structure_indices
+        ss=0
+        for ii in structure_indices:
+            aa=0
+            for jj in range(n_atoms):
+                coordinates[ii,jj,:]+=translation[ii,aa,:]
+                aa+=1
+            if not single_structure_translation:
+                ss+=1
 
-    elif (atom_indices is not None) and (structure_indices is None):
+    elif (structure_indices is None):
 
-        iter_atoms = atom_indices
-        iter_atoms = range(n_structures)
+        ss=0
+        for ii in range(n_structure):
+            aa=0
+            for jj in atom_indices:
+                coordinates[ii,jj,:]+=translation[ss,aa,:]
+                aa+=1
+            if not single_structure_translation:
+                s_t+=1
 
     else:
 
         iter_atoms = atom_indices
         iter_structures = structure_indices
 
-
-    for ii, s_t in zip(iter_structures, iter_structures_t):
-
-        if translation.shape[0]==1:
-            iter_atoms_t=repeat(0)
-        else:
-            iter_atoms_t=infinite_sequence(0,1)
-
-        for jj, a_t in zip(iter_atoms, iter_atoms_t):
-
-            coordinates[ii,jj,:]+=translation[s_t,a_t,:]
 
     pass
 
