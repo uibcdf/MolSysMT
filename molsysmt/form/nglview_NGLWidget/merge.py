4bd3167107b5298ee1f3725c01fe3c2fb320d85f
Diego Prada-Gracia <prada.gracia@gmail.com>
2023-06-26 22:12:59 -0600

    in process

--- molsysmt/form/nglview_NGLWidget/merge.py
+++ molsysmt/form/nglview_NGLWidget/merge.py
@@ -6,7 +6,8 @@ from molsysmt._private.variables import is_all
 def merge(items, atom_indices='all', structure_indices='all'):
 
     from . import to_molsysmt_MolSys
-    from ..molsysmt_MolSys import merge
+    from ..molsysmt_MolSys import merge as merge_molsysmt_MolSys
+    from ..molsysmt_MolSys import to_nglview_NGLWidget as molsysmt_MolSys_to_nglview_NGLWidget
 
     if is_all(atom_indices):
         atom_indices = ['all' for ii in range(len(items))]
@@ -14,9 +15,9 @@ def merge(items, atom_indices='all', structure_indices='all'):
     if is_all(structure_indices):
         structure_indices = ['all' for ii in range(len(items))]
 
-    items_molsysmt_MolSys = [to_molsysmt_MolSys(item) for item, atom in items]
-    merged_items_molsysmt_MolSys = merge(items_molsysmt_MolSys)
+    items_molsysmt_MolSys = [to_molsysmt_MolSys(item) for item, ii, jj in zip(items, atom_indices, structure_indices)]
+    merged_items_molsysmt_MolSys = merge_molsysmt_MolSys(items_molsysmt_MolSys)
+    merged_nglview_NGLWidget = molsysmt_MolSys_to_nglview_NGLWidget(merged_items_molsysmt_MolSys)
 
-
-    raise NotImplementedMethodError()
+    return merged_nglview_NGLWidget
 
