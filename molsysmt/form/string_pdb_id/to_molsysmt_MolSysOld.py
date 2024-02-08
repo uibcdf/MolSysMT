98fc3fd9398659064fbc6fdd79d35581abe6d1f0
Diego Prada <prada.gracia@gmail.com>
2024-02-08 17:59:51 -0600

    in process

--- molsysmt/form/string_pdb_id/to_molsysmt_MolSysOld.py
+++ molsysmt/form/string_pdb_id/to_molsysmt_MolSysOld.py
@@ -1,7 +1,7 @@
 from molsysmt._private.digestion import digest
 
 @digest(form='string:pdb_id')
-def to_molsysmt_MolSysOld(item, atom_indices='all', structure_indices='all'):
+def to_molsysmt_MolSysOld(item, atom_indices='all', structure_indices='all', skip_digestion=False):
 
     from . import to_mmtf_MMTFDecoder
     from ..mmtf_MMTFDecoder import to_molsysmt_MolSysOld as mmtf_MMTFDecoder_to_molsysmt_MolSysOld
--- molsysmt/form/string_pdb_id/to_molsysmt_StructuresOld.py
+++ molsysmt/form/string_pdb_id/to_molsysmt_StructuresOld.py
@@ -1,7 +1,7 @@
 from molsysmt._private.digestion import digest
 
 @digest(form='string:pdb_id')
-def to_molsysmt_StructuresOld(item, atom_indices='all', structure_indices='all'):
+def to_molsysmt_StructuresOld(item, atom_indices='all', structure_indices='all', skip_digestion=False):
 
     from . import to_mmtf_MMTFDecoder
     from ..mmtf_MMTFDecoder import to_molsysmt_StructuresOld as mmtf_MMTFDecoder_to_molsysmt_StructuresOld
