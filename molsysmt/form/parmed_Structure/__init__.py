f878e5be6d2cc0c2a6c3c87583a24a858e033db2
Diego Prada-Gracia <prada.gracia@gmail.com>
2024-02-20 23:40:59 -0600

    In process

--- molsysmt/form/parmed_Structure/__init__.py
+++ molsysmt/form/parmed_Structure/__init__.py
@@ -25,6 +25,9 @@ from .to_mdtraj_Trajectory import to_mdtraj_Trajectory
 from .to_nglview_NGLWidget import to_nglview_NGLWidget
 from .to_openmm_Modeller import to_openmm_Modeller
 from .to_openmm_Topology import to_openmm_Topology
+from .to_molsysmt_MolSys import to_molsysmt_MolSys
+from .to_molsysmt_Topology import to_molsysmt_Topology
+from .to_molsysmt_Structures import to_molsysmt_Structures
 from .to_molsysmt_MolSysOld import to_molsysmt_MolSysOld
 from .to_molsysmt_TopologyOld import to_molsysmt_TopologyOld
 from .to_molsysmt_StructuresOld import to_molsysmt_StructuresOld
@@ -39,6 +42,9 @@ _convert_to={
         'nglview.NGLWidget': to_nglview_NGLWidget,
         'openmm.Modeller': to_openmm_Modeller,
         'openmm.Topology': to_openmm_Topology,
+        'molsysmt.MolSys': to_molsysmt_MolSys,
+        'molsysmt.Topology': to_molsysmt_Topology,
+        'molsysmt.Structures': to_molsysmt_Structures,
         'molsysmt.MolSysOld': to_molsysmt_MolSysOld,
         'molsysmt.TopologyOld': to_molsysmt_TopologyOld,
         'molsysmt.StructuresOld': to_molsysmt_StructuresOld,
---
+++ molsysmt/form/parmed_Structure/to_molsysmt_Topology.py
@@ -0,0 +1,93 @@
+from molsysmt._private.digestion import digest
+import numpy as np
+from molsysmt.element.group.get_group_type import _get_group_type_from_group_name
+from molsysmt.element.atom.get_atom_type import _get_atom_type_from_atom_name
+
+@digest(form='parmed.Structure')
+def to_molsysmt_Topology(item, atom_indices='all', skip_digestion=False):
+
+    from molsysmt.native import Topology
+    from ..molsysmt_Topology import extract
+
+    tmp_item = Topology()
+
+    n_atoms = len(item.atoms)
+    n_groups = len(item.residues)
+    n_bonds = len(item.bonds)
+
+    tmp_item.reset_atoms(n_atoms=n_atoms)
+    tmp_item.reset_groups(n_groups=n_groups)
+    tmp_item.reset_bonds(n_bonds=n_bonds)
+
+    atom_index = 0
+    former_group_index = -1
+    former_chain_index = -1
+
+    aux_dict_chains={}
+
+    for atom in item.atoms:
+
+        group_index = atom.residue.idx
+
+        chain_id = atom.residue.chain
+        if chain_id not in aux_dict_chains:
+            aux_dict_chains[chain_id]=len(aux_dict_chains)
+        chain_index = aux_dict_chains[chain_id]
+
+
+        tmp_item.atoms.iloc[atom_index,0] = atom.idx
+        tmp_item.atoms.iloc[atom_index,1] = atom.name
+        tmp_item.atoms.iloc[atom_index,2] = _get_atom_type_from_atom_name(atom.name)
+        tmp_item.atoms.iloc[atom_index,3] = group_index
+        tmp_item.atoms.iloc[atom_index,4] = chain_index
+
+        if former_group_index!=group_index:
+            tmp_item.groups.iloc[group_index,0] = atom.residue.idx
+            tmp_item.groups.iloc[group_index,1] = atom.residue.name
+            tmp_item.groups.iloc[group_index,2] = _get_group_type_from_group_name(atom.residue.name)
+            former_group_index+=1
+
+        atom_index+=1
+
+    n_chains=len(aux_dict_chains)
+
+    tmp_item.reset_chains(n_chains=n_chains)
+
+    if len(aux_dict_chains)==1:
+        if '' in aux_dict_chains:
+            aux_dict_chains['A']=aux_dict_chains['']
+            del aux_dict_chains['']
+
+    for chain_id, chain_index in aux_dict_chains.items():
+        tmp_item.chains.iloc[chain_index,0] = chain_index
+        tmp_item.chains.iloc[chain_index,1] = chain_id
+
+    # bonds
+
+    bond_index = 0
+
+    for bond in item.bonds:
+
+        tmp_item.bonds.iloc[bond_index,0] = bond.atom1._idx
+        tmp_item.bonds.iloc[bond_index,1] = bond.atom2._idx
+
+        bond_index +=1
+
+    # components
+
+    tmp_item.rebuild_components()
+
+    ## molecules
+
+    tmp_item.rebuild_molecules()
+
+    ## entity
+
+    tmp_item.rebuild_entities()
+
+    ## extract if atom_indices is not 'all'
+
+    tmp_item = extract(tmp_item, atom_indices=atom_indices, copy_if_all=False, skip_digestion=True)
+
+    return tmp_item
+
